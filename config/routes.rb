26b7bb43154e50c7e580ea27b810ab33f6caa6b5
Jesse L Lawson <homestarflyer@gmail.com>
2016-08-17 13:29:30 -0400

    applied new design to old task logic

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  
+
   get 'pages/home'
 
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
